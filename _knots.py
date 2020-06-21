deb73e1e882b3f2194835a69e4ec58942ec480d6
Niklas Franke <jan.franke@tu-drotmund.de>
2020-06-21 19:21:08 +0200

    Implemented Bspline Calculation

--- _knots.py
+++ _knots.py
@@ -9,28 +9,28 @@ import math
 #################################
 
 
-def knots_average(u):
-    m = len(u) + 3 + 1
-    if ((m - 8) > 0):
-        v = np.zeros(m-8)
-        for i in range(0, m-8):
-            v[i] = (1/3) * sum([u[j] for j in range(i+1, i+4)])
-        t = np.concatenate((4*[0], v, 4*[1]), axis=None)
+def knots_average(u, k):
+    m = len(u) + k + 1
+    if ((m - (2*(k+1))) > 0):
+        v = np.zeros(m - (2*(k+1)))
+        for i in range(0, m - (2*(k+1))):
+            v[i] = (1/k) * sum([u[j] for j in range(i+1, i+k+1)])
+        t = np.concatenate(((k+1)*[0], v, (k+1)*[1]), axis=None)
         return t
     else:
-        return np.concatenate((4*[0], 4*[1]), axis=None)
+        return np.concatenate(((k+1)*[0], (k+1)*[1]), axis=None)
 
 
-def knots_uniform(u):
-    m = len(u) + 3 + 1
-    if ((m - 8) > 0):
-        v = np.zeros(m-8)
-        for i in range(1, m-8+1):
-            v[i-1] = i / (m-8+1)
-        t = np.concatenate((4*[0], v, 4*[1]), axis=None)
+def knots_uniform(u, k):
+    m = len(u) + k + 1
+    if ((m - (2*(k+1))) > 0):
+        v = np.zeros(m - (2*(k+1)))
+        for i in range(1, m - (2*(k+1))+1):
+            v[i-1] = i / (m - (2*(k+1))+1)
+        t = np.concatenate(((k+1)*[0], v, (k+1)*[1]), axis=None)
         return t
     else:
-        return np.concatenate((4*[0], 4*[1]), axis=None)
+        return np.concatenate(((k+1)*[0], (k+1)*[1]), axis=None)
 
 
 _switcher = {
@@ -39,10 +39,10 @@ _switcher = {
 }
 
 
-def calc_knots(u, k_type):
+def calc_knots(u, k, k_type):
     # Get the function from switcher dictionary
     func = _switcher.get(k_type)
     if(not func):
         raise NameError('Invalid Type for Generation of Knots')
     # Execute the function
-    return func(u)
+    return func(u, k)
--- spline.py
+++ spline.py
@@ -3,12 +3,13 @@
 
 from . import _parametrization
 from . import _knots
+from . import _helpers
 import numpy as np
 import math
 
 
 class Spline(object):
-    """Definition of a cubic B-Spline Curve in 2D.
+    """Definition of a B-Spline Curve in 2D.
 
     Parameters
     ----------
@@ -16,6 +17,8 @@ class Spline(object):
         Knots
     u : array_like, (n,)
         Parameters
+    k : int
+        Degree
 
     Attributes
     ----------
@@ -23,61 +26,122 @@ class Spline(object):
         Knot vector
     u : ndarray
         Parameter vector
+    k : int
+        Degree
     """
 
-    def __init__(self, t, u):
+    def __init__(self, t, u, k):
         super(Spline, self).__init__()
 
         self.t = np.asarray(t)
         self.u = np.asarray(u)
+        self.k = k
 
     @property
-    def tu(self):
-        return self.t, self.u
+    def tuk(self):
+        return self.t, self.u, self.k
 
     @classmethod
-    def construct_fast(cls, t, u):
+    def construct_fast(cls, t, u, k):
         """Construct a spline without making checks.
         Accepts same parameters as the regular constructor. Input arrays
         `t` and `u` must be of correct shape and dtype.
         """
         self = object.__new__(cls)
-        self.t, self.u = t, u
+        self.t, self.u, self.k = t, u, k
         return self
 
+    def calc_bspl(self, x):
+        """Compute non-zero BSplines at given point.
 
-def make_spline(points, p_type=0, k_type=0):
-    """Compute a cubic B-Spline Curve in 2D.
+        Parameters
+        ----------
+        x : float
+            The point at which the BSplines are to be evaluated
+
+        Returns
+        ----------
+        bspl : ndarray, shape (k+1,)
+            Non-zero BSplines at given point
+
+        Notes
+        -----
+        B-spline basis elements are defined via:
+
+        .. math::
+
+            B_{i, 0}(x) = 1, \textrm{if $t_i \le x < t_{i+1}$, otherwise $0$,}
+            B_{i, k}(x) = \frac{x - t_i}{t_{i+k} - t_i} B_{i, k-1}(x)
+                    + \frac{t_{i+k+1} - x}{t_{i+k+1} - t_{i+1}} B_{i+1, k-1}(x)
+
+        References
+        ----------
+        .. [1] Carl de Boor, A practical guide to splines, Springer, 2001.
+        """
+        if ((x < self.t[0]) or (x > self.t[len(self.t)-1])):
+            raise TypeError('Extrapolation is not supported')
+        index = _helpers.search_index(self.t, x)
+        bspl = np.zeros(self.k+1)
+
+        bspl[0] = 1
+        for j in range(self.k):
+            saved = 0
+            start = max(j - index, 0)
+            stop = min(len(self.t) - index - 1, j+1)
+            for r in range(start, stop):
+                deltar = self.t[index+r+1] - x
+                deltal = x - self.t[index-j+r]
+                term = bspl[r] / (deltar + deltal)
+                if ((index - (j+1)) + r) <= -1:
+                    bspl[r] = 0
+                else:
+                    bspl[r] = saved + deltar * term
+                saved = deltal * term
+            if index > (len(self.t) - (j+1) - 2):
+                bspl[j+1] = 0
+            else:
+                bspl[j+1] = saved
+        beg = max(self.k - index, 0)
+        end = min(len(self.t) - index - 1, self.k+1)
+        return bspl[beg:end]
+
+
+def make_spline(points, p_type=0, k_type=0, k=3):
+    """Compute a B-Spline Curve in 2D.
 
     Parameters
     ----------
     points : array_like (2, n)
-        A list of sample vector arrays representing the curve in 2D.
+        A list of sample vector arrays representing the curve in 2D
     p_type : int, optional
         See documentation of `_generate_param`
     k_type : int, optional
         See documentation of `_generate_knots`
+    k : int, optional
+        Degree of the BSpline curve
 
     Returns
     ----------
     spl :
-        a cubic BSpline object.
+        BSpline object.
     """
-    if (len(points[0]) <= 3) or (len(points[1]) <= 3):
-        raise TypeError('n > 3 must hold')
+    if (len(points[0]) <= k) or (len(points[1]) <= k):
+        raise TypeError('n > k must hold')
     if (len(points[0]) != len(points[1])):
         raise TypeError('Number of coordinates for points are unequal')
+    if (k < 0):
+        raise TypeError('Negative degree is not possible')
     x = np.asarray(points[0])
     y = np.asarray(points[1])
 
     u = _generate_param(x, y, p_type)
-    t = _generate_knots(u, k_type)
+    t = _generate_knots(u, k, k_type)
 
-    return Spline.construct_fast(t, u)
+    return Spline.construct_fast(t, u, k)
 
 
 def _generate_param(x, y, p_type=0):
-    """Compute the parameters of a cubic B-Spline Curve in 2D.
+    """Compute the parameters of a B-Spline Curve in 2D.
 
     Parameters
     ----------
@@ -86,7 +150,7 @@ def _generate_param(x, y, p_type=0):
     y : array_like, shape (n,)
         Ordinate
     p_type : int, optional
-        Determines the method for calculating the parametrization vector.
+        Determines the method for calculating the parametrization vector
 
         * if p_type=0, identically spaced method
         * if p_type=1, uniform spaced method
@@ -103,13 +167,15 @@ def _generate_param(x, y, p_type=0):
     return _parametrization.calc_param(x, y, p_type)
 
 
-def _generate_knots(u, k_type=0):
-    """Compute the knots of a cubic B-Spline Curve in 2D.
+def _generate_knots(u, k, k_type=0):
+    """Compute the knots of a B-Spline Curve in 2D.
 
     Parameters
     ----------
     u : array_like, shape (n,)
         Parameters
+    k : int
+        Degree
     u_type : int, optional
         Determines the method for calculating the parametrization vector.
 
@@ -123,4 +189,4 @@ def _generate_knots(u, k_type=0):
     t : ndarray, shape (n+3+1,)
         Knot vector
     """
-    return _knots.calc_knots(u, k_type)
+    return _knots.calc_knots(u, k, k_type)
