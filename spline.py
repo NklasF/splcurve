a3ccb9dbba4a68f09c136542a6d5bf64fcdace9b
Niklas Franke <jan.franke@tu-dortmund.de>
2020-06-21 19:49:36 +0200

    Added Comments

--- spline.py
+++ spline.py
@@ -81,26 +81,31 @@ class Spline(object):
         if ((x < self.t[0]) or (x > self.t[len(self.t)-1])):
             raise TypeError('Extrapolation is not supported')
         index = _helpers.search_index(self.t, x)
+        # Initialize bspl with k+1 entries for intermediate values, it may get sliced later
         bspl = np.zeros(self.k+1)
 
         bspl[0] = 1
         for j in range(self.k):
             saved = 0
+            # Indicates to skip some iterations if necessary
             start = max(j - index, 0)
             stop = min(len(self.t) - index - 1, j+1)
             for r in range(start, stop):
                 deltar = self.t[index+r+1] - x
                 deltal = x - self.t[index-j+r]
                 term = bspl[r] / (deltar + deltal)
+                # Indicates an invalid Bspline, but prepares right term for next iteration
                 if ((index - (j+1)) + r) <= -1:
                     bspl[r] = 0
                 else:
                     bspl[r] = saved + deltar * term
                 saved = deltal * term
+            # Indicates an invalid Bspline and sets it to 0
             if index > (len(self.t) - (j+1) - 2):
                 bspl[j+1] = 0
             else:
                 bspl[j+1] = saved
+        # Slice bspl to contain only valid Bsplines
         beg = max(self.k - index, 0)
         end = min(len(self.t) - index - 1, self.k+1)
         return bspl[beg:end]
