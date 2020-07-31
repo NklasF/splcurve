f719cdf8d9087c20b6dff4225fafa6f4cd548828
Niklas Franke <jan.franke@tu-dortmund.de>
2020-07-31 18:57:21 +0200

    Implemented LU Slover

--- _algebra.py
+++ _algebra.py
@@ -9,16 +9,56 @@ import math
 #################################
 
 
-def back_sub(b, A):
-    """Finds a solution to the linear system Ax=b,
-    if A is an upper triangular matrix.
+def LU_solve(b, LU):
+    """Finds a solution to the linear system Ax=b
+    with A factorized into LU.
     """
-    n = len(b)-1
-    x = np.zeros(n+1)
-    x[n] = b[n] / A[n, n]
-    for k in range(n-1, -1, -1):
-        s = 0
-        for j in range(k+1, n+1):
-            s += (A[k, j] * x[j])
-        x[k] = (b[k] - s) / A[k, k]
+    if (len(LU[0]) != len(LU[1])):
+        raise ValueError('Matrix must be square!')
+    if (len(LU[0]) != len(b)):
+        raise ValueError('Matrix and Right-Side must be of same dimension!')
+    n = len(b)
+
+    # Solving Ly=b
+    y = np.zeros(n)
+    for i in range(n):
+        sum = 0
+        for k in range(i):
+            sum += LU[i, k]*y[k]
+        y[i] = b[i] - sum
+
+    # Solving Ux=y
+    x = np.zeros(n)
+    for i in range(n-1, -1, -1):
+        sum = 0
+        for k in range(i+1, n):
+            sum += LU[i, k] * x[k]
+        x[i] = (y[i] - sum) / LU[i, i]
     return x
+
+
+def LU_fac(A):
+    """Factorization of A according to A=LU without pivoting.
+    L is an upper-triangular matrix.
+    U is an lower-triangular matrix.
+    LU=L+U-I
+    """
+    if (len(A[0]) != len(A[1])):
+        raise ValueError('Matrix must be square!')
+    n = len(A[0])
+    LU = np.zeros((n, n))
+    # Factorization of A
+    for i in range(n):
+        # Calculate ith row
+        for j in range(i, n):
+            sum = 0.0
+            for k in range(i):
+                sum += LU[i, k]*LU[k, j]
+            LU[i, j] = A[i, j]-sum
+        # Calculate ith column
+        for j in range(i+1, n):
+            sum = 0.0
+            for k in range(i):
+                sum += LU[j, k]*LU[k, i]
+            LU[j, i] = (A[j, i]-sum)/LU[i, i]
+    return LU
